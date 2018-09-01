8d59ec824ce0f98c20ec7f0b3e3980ba43df71a0
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2018-09-01 21:22:12 +0200

    add validation to user model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,26 @@
 class User < ApplicationRecord
+  # == Constants ============================================================
+  enum role: { admin: 'admin', member: 'member' }
+
+  # == Attributes ===========================================================
+
+  # == Extensions ===========================================================
+  has_secure_password
+
+  # == Relationships ========================================================
+
+  # == Validations ==========================================================
+  validates_presence_of     :email
+  validates_presence_of     :username
+  validates_uniqueness_of   :email
+  validates_uniqueness_of   :username
+  validates_length_of :password, maximum: 72, minimum: 8, allow_nil: false, allow_blank: false
+  # == Scopes ===============================================================
+
+  # == Callbacks ============================================================
+
+  # == Methods ========================================================
+  before_validation { 
+        (self.email = self.email.to_s.downcase) && (self.username = self.username.to_s.downcase) 
+    }
 end
--- db/migrate/20180901190138_create_users.rb
+++ db/migrate/20180901190138_create_users.rb
@@ -1,9 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
+      t.string :username, null: false, unique: true
+      t.string :email, null: false, index: true, unique: true
       t.string :password_digest
+      t.string :role, null: false, default: 'member'
 
       t.timestamps
     end
