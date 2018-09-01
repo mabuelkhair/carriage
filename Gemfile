bbb2d3b7f644142eb21fb7fea33a03509fa39265
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2018-09-01 21:36:39 +0200

    add simplecommand gem and add token field to user model

--- Gemfile
+++ Gemfile
@@ -30,7 +30,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 # to handle json web tokens
 gem 'jwt'
-
+# to add service (aka commands)
+gem 'simple_command'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_01_190138) do
+ActiveRecord::Schema.define(version: 2018_09_01_192744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_09_01_190138) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
   end
 
 end
