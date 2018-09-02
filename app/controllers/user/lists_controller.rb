082b6c04f85826f2b274fb1c33d70287c1ac93d6
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2018-09-02 18:14:59 +0200

    fix validation, user should be member in list to show

--- app/controllers/user/lists_controller.rb
+++ app/controllers/user/lists_controller.rb
@@ -1,6 +1,6 @@
 class User::ListsController < ApplicationController
   before_action :set_list, only: [:show]
-  before_action :is_list_owner, only: [:show]
+  before_action :is_list_member, only: [:show]
 
   # GET /lists
   def index
@@ -24,8 +24,8 @@ class User::ListsController < ApplicationController
       params.require(:list).permit(:title)
     end
 
-    def is_list_owner
-      render json: { error: 'You do not have permission for this' }, status: 403 unless @list.owner_id==@current_user.id
+    def is_list_member
+      @list.users.find(@current_user.id)
     end
 
 end
