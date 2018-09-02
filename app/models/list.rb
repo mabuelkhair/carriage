ee4bb4e64eb3c90b2108a7b91a1c484d7785bf1f
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2018-09-02 16:25:16 +0200

    add cards list relation

--- app/models/list.rb
+++ app/models/list.rb
@@ -9,6 +9,7 @@ class List < ApplicationRecord
   # == Relationships ========================================================
   belongs_to :owner ,:class_name=>'User'
   has_and_belongs_to_many :users
+  has_many :cards
   # == Validations ==========================================================
   validates_length_of :title, maximum: 100, allow_nil: false, allow_blank: false
   # == Scopes ===============================================================
