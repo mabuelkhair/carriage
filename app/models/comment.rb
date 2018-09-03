8cb673a74010125b1aaf34135e58dfc5e8baea4b
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2018-09-03 19:09:20 +0200

    add comments to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,24 @@
 class Comment < ApplicationRecord
+
+  # == Constants ============================================================
+
+  # == Attributes ===========================================================
+
+  # == Extensions ===========================================================
+
+  # == Relationships ========================================================
   belongs_to :card
   belongs_to :owner ,:class_name=>'User'
   has_many :replies, class_name: "Comment",
                           foreign_key: "comment_id"
  
   belongs_to :comment, class_name: "Comment", optional: true
+
+  # == Validations ==========================================================
+
+  # == Scopes ===============================================================
+
+  # == Callbacks ============================================================
+
+  # == Methods ========================================================
 end
