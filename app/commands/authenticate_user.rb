2ef3aa9f767d97f9ad448ea8d8d0fec970ea2e4c
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2018-09-06 01:59:04 +0200

    set expiration of token in redis

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -9,9 +9,10 @@ class AuthenticateUser
   def call
     user_obj = user
     if user_obj
+      expiration_time = 1.week.from_now
       token = JsonWebToken.encode(user_id: user_obj.id,role: user_obj.role)
       user_obj.update_attribute(:token,token)
-      $redis.set("user:#{user_obj.id}",token)
+      $redis.set("user:#{user_obj.id}", token, options = {ex: expiration_time.to_i - Time.now.to_i})
     end
     token
   end
