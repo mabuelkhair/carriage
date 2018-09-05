686529f8260decb6dc0601e6cc2b1ec83a06ae23
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2018-09-05 20:37:57 +0200

    test list attributes

--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    def setup
+        @owner = User.create(username:"user1", password: "123123123", email: "mail@example.com")
+    end
+    test "should not save list without title" do
+      list = List.new(owner: @owner)
+      assert_not list.save, "Saved the list without a title"
+    end
+
+    test "should not save list without owner" do
+      list = List.new(title: "first list")
+      assert_not list.save, "Saved the list without owner"
+    end
+
+    test "valid data" do
+      list = List.new(title: "first list",owner: @owner)
+      assert list.save, "does not save the list with valid data"
+
+    end
+
 end
