d94f8aa0cc2eb462d23022e94774e72be4b924ae
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2018-09-05 21:03:38 +0200

    test card model attributes

--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class CardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @owner = User.create(username:"user1", password: "123123123", email: "mail@example.com")
+    @list = List.new(title: "first list",owner: @owner)
+  end
+  test "should not save card without title" do
+    card = Card.new(owner: @owner, list: @list, description: "lorem ipsum")
+    assert_not card.save, "Saved the card without a title"
+  end
+
+  test "should not save card without description" do
+    card = Card.new(owner: @owner, list: @list, title: "lorem ipsum")
+    assert_not card.save, "Saved the card without a description"
+  end
+
+  test "should not save card without list" do
+    card = Card.new(owner: @owner, title: "lorem ipsum", description: "lorem ipsum")
+    assert_not card.save, "Saved the card without a list"
+  end
+
+  test "should not save card without owner" do
+    card = Card.new(list: @list, title: "lorem ipsum", description: "lorem ipsum")
+    assert_not card.save, "Saved the card without a owner"
+  end
+
+  test "valid data" do
+    card = Card.new(owner: @owner, list: @list, title: "lorem ipsum", description: "lorem ipsum")
+    assert card.save, "deos not save valid card"
+  end
+
 end
