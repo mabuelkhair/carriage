83b9694a1ce53d41ad019cc65a356c99660aec9e
mabuelkhair <mabuelkhair94@gmail.com>
2018-09-06 12:14:56 +0200

    admin list all lists including user lists

--- test/integration/admin_flow_test.rb
+++ test/integration/admin_flow_test.rb
@@ -6,6 +6,7 @@ class AdminFlowTest < ActionDispatch::IntegrationTest
     @list = List.create(title: "test list",owner: @admin)
     @card = Card.create(owner: @admin, list: @list, title: "lorem ipsum", description: "lorem ipsum")
     @user =  User.create(username:"user1", password:"123456789", email:"user@test.com")
+    @user_list = List.create(title: "test user list",owner: @user)
     # @current_user = users(:admin)
   end
   
@@ -51,6 +52,15 @@ class AdminFlowTest < ActionDispatch::IntegrationTest
     delete "/admin/lists/#{@list.id}/unassign_member", headers:{"Authorization" => login, 'CONTENT_TYPE' => 'application/json'},
        params:  params.to_json
     assert_response :success
+  end
+
+  test "get all lists" do
+    get "/admin/lists/", headers:{"Authorization" => login, 'CONTENT_TYPE' => 'application/json'}
+    assert_response :success
+    lists = JSON.parse @response.body
+    assert lists.any? {|list| list['id'] == @user_list.id}
+
+
   end
 
   def login()
